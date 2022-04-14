2e16a5fded30f4c75e0b6a5015f1bda3cf1568ef
stutisk <48529714+stutisk@users.noreply.github.com>
2022-04-14 16:51:53 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import "./App.css";
+import {Homepage} from "./Pages/Homepage/Homepage";
+import {Productpage} from "./Pages/Productpage/Productpage";
+// import {MockAPI} from "./Pages/Mockman/Mockman"
+import { Routes, Route} from "react-router-dom";
 
 
 function App() {
   return (
     <div className="App">
-     
+     <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/productpage" element={<Productpage />} />
+      </Routes>
     </div>
   );
 }
 
 export default App;
+
+
+// import React from "react";
+// import Mockman from "mockman-js";
+// function MockAPI() {
+//   return (
+//     <div className="MockAPI">
+//       <Mockman />
+//     </div>
+//   );
+// }
+
+// export default MockAPI;
